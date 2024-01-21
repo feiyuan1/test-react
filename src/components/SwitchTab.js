0d1101390da2454ed1cd5c1ba4bb5941c1b37ffc
feiyuan peng <feiyuan2987@163.com>
2024-01-21 16:12:10 +0800

    fix: 通过 useDeferredValue 让 tab 切换更加顺滑02

--- src/components/SwitchTab.js
+++ src/components/SwitchTab.js
@@ -8,7 +8,7 @@ export default function SwitchTab(){
   const [isPrintTab, setPrintTab] = useState(true)
   const deferredPrintTab = useDeferredValue(isPrintTab)
 
-  return  (<div>
+  return  (<div style={{opacity: isPrintTab === deferredPrintTab ? 1 : 0.7}}>
   {deferredPrintTab ? <Print /> : <Test />}
 <button onClick={() => {setPrintTab(old => !old)}}>Switch</button>
 </div>)
