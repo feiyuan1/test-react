fd5c64a93bd5a0fa6213bed6da69128ade4e7d85
feiyuan peng <feiyuan2987@163.com>
2024-01-21 16:03:53 +0800

    fix: 通过 useDeferredValue 让 tab 切换更加顺滑

--- src/DemoWithSuspense.js
+++ src/DemoWithSuspense.js
@@ -1,4 +1,4 @@
-import {lazy, Suspense, useState} from 'react'
+import {lazy, Suspense, useDeferredValue, useState} from 'react'
 import Loading from './components/Loading.js'
 import lazyLoad from './lazyLoad.js'
 
@@ -7,12 +7,12 @@ const Test = lazy(() => lazyLoad(import('./components/Test.js')))
 
 export default function DemoWithSuspense(){
   const [isPrintTab, setPrintTab] = useState(true)
+  const deferredPrintTab = useDeferredValue(isPrintTab)
 
   return <div>
      <Suspense fallback={<Loading />}>
-    {isPrintTab ? <Print /> : <Test />}
+    {deferredPrintTab ? <Print /> : <Test />}
   </Suspense>
 <button onClick={() => {setPrintTab(old => !old)}}>Switch</button>
     </div>
-  
 }
