d89b9ff47f9a2e0e3b92d70077fd35a1a2fce3a8
feiyuan peng <feiyuan2987@163.com>
2024-04-10 18:55:52 +0800

    test: key keyword

---
+++ src/DemoForKey/Child.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Child extends Component{
+  componentWillUnmount(){
+    console.log('unmount: ', this.props.text)
+  }
+  render(){
+    return <div>{this.props.text}
+    <input />
+    </div>
+  }
+}
+
+export default Child
